9ae941d4d63bee3880d539f30d7476fa5f3485a9
Ermi24et <lilermi24@gmail.com>
2024-03-28 23:54:06 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,6 +8,15 @@ status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_file_size = 0
 line_count = 0
 
+
+def print_op():
+        """
+        prints all
+        """
+        print("File size:", total_file_size)
+        for k, v in status_codes.items():
+            if v:
+                print(f"{k}: {v}")
 try:
     for line in sys.stdin:
         line_count += 1
@@ -29,11 +38,3 @@ try:
     print_op()
 except KeyboardInterrupt:
         print_op()
-def print_op():
-        """
-        prints all
-        """
-        print("File size:", total_file_size)
-        for k, v in status_codes.items():
-            if v:
-                print(f"{k}: {v}")
