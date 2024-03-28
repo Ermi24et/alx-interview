85844da912ebb79a79c656a690bc21253dd11633
Ermi24et <lilermi24@gmail.com>
2024-03-29 00:00:13 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,19 +4,22 @@ A script that reads stdin line by line and computes metrics
 """
 import sys
 
+
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_file_size = 0
 line_count = 0
 
 
 def print_op():
-        """
-        prints all
-        """
-        print("File size:", total_file_size)
-        for k, v in status_codes.items():
-            if v:
-                print(f"{k}: {v}")
+    """
+    prints all
+    """
+    print("File size:", total_file_size)
+    for ke, val in status_codes.items():
+        if val:
+            print("{}: {}".format(ke, val))
+
+
 try:
     for line in sys.stdin:
         line_count += 1
@@ -28,11 +31,11 @@ try:
             continue
         try:
             status_code = int(line[-2])
-            if status_code in status_codes:
+            if status_code in status_codes.keys():
                 status_codes[status_code] += 1
         except (IndexError, ValueError, TypeError):
             continue
-        if count == 10:
+        if line_count == 10:
             print_op()
             line_count = 0
     print_op()
