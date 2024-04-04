a781758d045dfe15929d4656f1a479b4428f6d95
Ermi24et <lilermi24@gmail.com>
2024-04-04 22:48:02 +0300

    utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """a method that determines if a given data set represents a valid UTF-8
+    encoding"""
+    count_ = 0
+
+    for byte in data:
+        if count_ == 0:
+            if byte >> 3 == 0b11110:
+                count_ = 3
+            elif byte >> 4 == 0b1110:
+                count_ = 2
+            elif byte >> 5 == 0b110:
+                count_ = 1
+            elif byte >> 7 == 0b0:
+                continue
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            count_ -= 1
+
+    return count_ == 0
