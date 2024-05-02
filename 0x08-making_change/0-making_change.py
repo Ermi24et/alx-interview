d159f9b9936f9cca0aee950549e87ef622166616
Ermi24et <lilermi24@gmail.com>
2024-05-02 22:19:06 +0300

    change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,24 +5,17 @@ import sys
 
 def makeChange(coins, total):
     """returns fewest number of coins needed to meet total"""
-    n = len(coins)
-    if (total <= 0):
+    if total <= 0:
         return (0)
-    table = [0 for i in range(total + 1)]
-
-    table[0] = 0
-
-    for i in range(1, total + 1):
-        table[i] = sys.maxsize
-
-    for i in range(1, total + 1):
-        for j in range(n):
-            if (coins[j] <= i):
-                sub_sol = table[i - coins[j]]
-                if (sub_sol != sys.maxsize and sub_sol + 1 < table[i]):
-                    table[i] = sub_sol + 1
-
-    if table[total] == sys.maxsize:
+    
+    coins = sorted(coins)[::-1]
+    i = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        while total >= coin:
+            total -= coin
+            i += 1
+    if total:
         return (-1)
-
-    return table[total]
+    return (i)
