42088d2a835490a5dc6a48325267e8c5e247d090
Ermi24et <lilermi24@gmail.com>
2024-05-02 22:19:59 +0300

    change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ def makeChange(coins, total):
     """returns fewest number of coins needed to meet total"""
     if total <= 0:
         return (0)
-    
+
     coins = sorted(coins)[::-1]
     i = 0
     for coin in coins:
