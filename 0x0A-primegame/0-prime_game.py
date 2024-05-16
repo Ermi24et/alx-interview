5133ac7e20be122bcbaaf35ec18a19d1fd12a99f
Ermi24et <lilermi24@gmail.com>
2024-05-16 16:56:30 +0300

    prime game challenge accepted

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Prime Number
+"""
+
+
+def check_prime(n):
+    """checks and returns the primes of a number"""
+    primes = []
+    elemets = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (elemets[p]):
+            primes.append(prime)
+            for i in range(prime, n + 1, prime):
+                elemets[i] = False
+    return primes
+
+
+def isWinner(x, nums):
+    """returns the name of the player that won the most rounds"""
+    Ben = Maria = 0
+    for i in range(x):
+        primes = check_prime(nums[i])
+        if (len(primes) % 2 == 0):
+            Ben += 1
+        else:
+            Maria += 1
+    if Maria > Ben:
+        return "Maria"
+    elif Ben > Maria:
+        return "Ben"
+    else:
+        return None
